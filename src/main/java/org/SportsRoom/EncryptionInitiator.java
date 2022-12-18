23131f60c9d0d2f6c28b5475b28bf1ed95b9147a
Alper Durmus <alper.durmus@ug.bilkent.edu.tr>
2022-12-18 22:15:50 +0300

    Write The Inititator Class
    All writable code of the Encryption Initiator class is done. The rest of the code needs the functionality of sending and receiving messages

--- src/main/java/org/SportsRoom/EncryptionInitiator.java
+++ src/main/java/org/SportsRoom/EncryptionInitiator.java
@@ -10,13 +10,26 @@ public class EncryptionInitiator {
 	private static long publicKey;
 	private static long privateKey;
 
-	public long CreateSharedKey() {
-		throw new UnsupportedOperationException("The method is not implemented yet.");
+	public long CreateSharedKey() throws FileNotFoundException {
+		long[] primes = new long[2];
+		primes = getRandomPrimeNumberPair();
+		//TODO: Write a line of code which transmitts the common primes.
+		//-----------------------------------------
+		//Example: Messenger.send(theReceiver,primes);
+		//-----------------------------------------
+		long stage1 = pow(primes[1],privateKey)%primes[0];
+		//TODO: Write a line of code which transmitts the stage 1 result of the Initiation.
+		//-----------------------------------------
+		//Example: Messenger.send(theReceiver,stage1);
+		//-------------------------------------------
+		//TODO: Write the necessary code to obtain the Receiver's stage 1 result.
+		long receiverStg1;
+		return pow(receiverStg1,privateKey)%primes[0];
+
 	}
 
 	private long[] getRandomPrimeNumberPair() throws FileNotFoundException {
 		File file = new File("../resources/Output.txt");
-        Scanner userIn = new Scanner(System.in);
         Scanner fileIn = new Scanner(file);
         ArrayList<Long> primes = new ArrayList<Long>();
         ArrayList<Long> allPrimes = new ArrayList<Long>();
@@ -47,6 +60,13 @@ public class EncryptionInitiator {
 		long first = primes.get((int) Math.random()*primes.size());
 		long second = roots.get(primes.indexOf(first)).get((int) Math.random()*roots.get(primes.indexOf(first)).size());
 		long[] result = {first,second};
+		return result;
+	}
+	private static long pow(long a , long b){
+		long result = 1;
+		for(int i = 0; i<b;i++){
+			result = result*a;
+		}
 		return result;
 	}
 
