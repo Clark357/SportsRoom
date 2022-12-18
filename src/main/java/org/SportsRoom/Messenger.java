4ada193d171b34e568cc34447f88106b3126e9c9
Mehmet Can Bıyık <can.biyik@ug.bilkent.edu.tr>
2022-12-18 17:46:26 +0300

    Add various features for GUI and Decentralization
    - Deleted an extra package import from News class
    - Deleted some of the test code from Main class
    
    + Added a dialog box for Managing Supergroups and implemented the required methods for it in MainWindow.ManageGroups
    + Added a protocol for updating the user roles
    + Added accessor methods to InitiationProtocolMessage
    + Added accessor methods for LoginWindow's username and password fields
    + Added methods to change the role in User class
    + Added new property and implemented accessor classes to SynchronizationProtocolMessage
    + Added new class RoleUpdateProtocolMessage
    + Added a Receiver implementation to Messenger class
    + Added the synchronization, message sending and user role update methods to Messenger class
    + Added class MetaSuperGroup that will be responsible for Supergroup initialization
    + Added some exception handling and accessor methods to GUI elements
    + Added partial MenuBar implementation for GUI
    + Added the News class functionality to GUI
    
    ~ Changed the pom.xml to include jgroups and flatlaf packages
    ~ Made ChatMessage class Serializable
    ~ Changed the way LoginWindow interacted with the MainWindow now the MainWindow will only open when the login operation happened
    ~ Changed the MainMenu to include the News in a TextPane
    ~ Changed accessor method names in User class
    ~ Made User class Serializable

--- src/main/java/org/SportsRoom/Messenger.java
+++ src/main/java/org/SportsRoom/Messenger.java
@@ -1,13 +1,11 @@
 package org.SportsRoom;
 
-import org.jgroups.JChannel;
+import org.jgroups.*;
 
-import java.io.*;
+import java.time.LocalDateTime;
 import java.util.*;
-import java.awt.*;
-import javax.swing.*;
 
-public class Messenger {
+public class Messenger implements Receiver{
 
 	private JChannel superGroup;
 	private Storage chatStorage;
@@ -15,16 +13,55 @@ public class Messenger {
 	private ArrayList<ChatMessage> message;
 	private MessengerListener listener;
 
-	public Messenger(String groupName ,MessengerListener listener) {
+	public void receive(Message msg) { //TODO: Check whether it works
+		if(msg.getObject() instanceof ChatMessage) { //TODO: Should be decrypted but for now there is no encryption
+			listener.eventHappened(msg.getObject());
+		} else if(msg.getObject() instanceof RoleUpdateProtocolMessage) {
+			users = new ArrayList<>(Arrays.asList(((RoleUpdateProtocolMessage)msg.getObject()).getUsers()));
+		} else if(msg.getObject() instanceof SynchronizationProtocolMessage) {
+			if(((SynchronizationProtocolMessage)msg.getObject()).isRequest())
+				superGroup.send(new ObjectMessage(msg.getSrc(), new SynchronizationProtocolMessage(LocalDateTime.now(),
+																							false,
+																									chatStorage.getMessages(((SynchronizationProtocolMessage)msg.getObject()).getDate(),
+																									(User[])chatStorage.getUsers().toArray(),
+																									superGroup.getClusterName()))));
+			else {
+				SynchronizationProtocolMessage history = (SynchronizationProtocolMessage) msg.getObject();
+				chatStorage.addMessages(history.getMessages());
+				for(ChatMessage c : history.getMessages())
+					listener.eventHappened(c);
+				//TODO:Update user information
+			}
+		}
+	}
+
+	public void viewAccepted(View v) {
+		//TODO: Decide whether a view update needs any action
+	}
+
+	// NOTE: It is assumed that whenever this constructor is called the supergroup is already initialized
+	public Messenger(String groupName ,MessengerListener listener) throws Exception { //TODO: Finish the group reconnection constructor by communicating with Storage
+		superGroup = new JChannel().setName(MetaSuperGroup.metaSuperGroup.getName()).connect(groupName).setReceiver(this);
 		this.listener = listener;
+
+		chatStorage = new Storage(groupName);
+
+		synchronizeHistory();
+
+		for(User u: chatStorage.getUsers())
+			listener.eventHappened(u);
 	}
 
 	public void synchronizeHistory() {
-		throw new UnsupportedOperationException("The method is not implemented yet.");
+		superGroup.send(new ObjectMessage(superGroup.getView().getMembers().get(0), new SynchronizationProtocolMessage(chatStorage.getMessages(1), true, null, null, superGroup.getClusterName())));
 	}
 
-	public void sendMessage() {
-		throw new UnsupportedOperationException("The method is not implemented yet.");
+	public void sendMessage(ChatMessage msg) throws Exception {
+		superGroup.send(new ObjectMessage(null, msg));
+	}
+
+	public void updateRoles() throws Exception {
+		superGroup.send(new ObjectMessage(null, new RoleUpdateProtocolMessage((User[])users.toArray())));
 	}
 
 	private void createSuperGroup() {
@@ -33,5 +70,34 @@ public class Messenger {
 
 	public ArrayList<User> getUsers() {
 		return users;
+	}
+}
+
+class MetaSuperGroup implements Receiver { //TODO: Group creation function that uses encryption initiator
+	static JChannel metaSuperGroup;
+	private static MainWindow w;
+
+	public static void initMetaSuperGroup(String username, MainWindow window) {
+		w = window;
+		try {
+			metaSuperGroup = new JChannel().setName(username).setReceiver(new MetaSuperGroup()).connect("MetaSuperGroup");
+		} catch(Exception e) {
+			System.err.println("Fatal Error: Could not connect to MetaSuperGroup.");
+			System.exit(-1);
+		}
+	}
+
+	public static void initSuperGroup(String groupName, User[] users, String username, String password) {//TODO: Implement this method
+		throw new UnsupportedOperationException("This will be implemented when EncryptionInitiator class is implemented");
+	}
+
+	public void receive(Message msg) {
+		InitiationProtocolMessage m = (InitiationProtocolMessage) msg.getObject();
+
+		w.getPanel().addChatPanel(new MainWindow.ChatPanel(m.getGroupName(), m.isCanSubmit(), new User(metaSuperGroup.getName(), metaSuperGroup.getAddressAsUUID(), m.isCanSubmit())));
+	}
+
+	public void viewAccepted(View v) {
+		//TODO: Decide whether a view update needs any action
 	}
 }
