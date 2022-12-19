d7a836349b1f1c5d5b3ae36362692ff929f441d0
Alper Durmus <alper.durmus@ug.bilkent.edu.tr>
2022-12-19 09:55:53 +0300

    Write the Encryption & Decryption Function
    The Encryption and the Decryption functions are written completely and also tested

--- src/main/java/org/SportsRoom/Encryption.java
+++ src/main/java/org/SportsRoom/Encryption.java
@@ -1,13 +1,27 @@
 package org.SportsRoom;
 
+import java.util.ArrayList;
+
 public class Encryption {
 
-	public String Encrypt(String message, long sharedKey) {
-		throw new UnsupportedOperationException("The method is not implemented yet.");
+	public static String Encrypt(String message, long sharedKey) {
+		char[] allChars = message.toCharArray();
+		String result = "";
+        for(char x: allChars){
+         	int y = x;
+        	result=result+(y*sharedKey)+"\n";
+		}
+		return result;
 	}
 
-	public String Decrypt(String message, long sharedKey) {
-		throw new UnsupportedOperationException("The method is not implemented yet.");
+	public static String Decrypt(String message, long sharedKey) {
+		String[] lol = message.split("\n");
+		String result ="";
+		for(String k : lol){
+			char x = (char)((Long.parseLong(k))/sharedKey);
+			result += x;
+		}
+		return result;
 	}
 
 }
