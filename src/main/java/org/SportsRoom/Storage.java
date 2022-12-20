29dc1b0f3b007224d5868588d7d07a9c4203a19d
Umut <basar.demir@ug.bilkent.edu.tr>
2022-12-20 22:41:33 +0300

    Do not merge

--- src/main/java/org/SportsRoom/Storage.java
+++ src/main/java/org/SportsRoom/Storage.java
@@ -116,7 +116,7 @@ public class Storage {
 		hash = "" + publicKey + privateKey;
 		try {
 			infoRaf.seek(0);
-			infoRaf.writeBytes(Encryption.Encrypt( "" + sharedKey, hash.hashCode()) + "\n");
+			infoRaf.writeBytes(mapper.writeValueAsString(Encryption.Encrypt("" + sharedKey, hash.hashCode()))+ "\n");
 			infoRaf.close();
 			raf.writeBytes(mapper.writeValueAsString(new ChatMessage(LocalDateTime.parse("2000-01-01T01:01:01"), new User("SportsRoom", "0",Role.MODERATOR), Encryption.Encrypt("This is the start of your conversation",sharedKey))) + "\n*****");
 			isInitialized = true;
@@ -292,7 +292,9 @@ public class Storage {
 
 		RandomAccessFile infoRaf;
 		long key;
+		String hash;
 
+		hash = "" + publicKey + privateKey;
 		try {
 			infoRaf = new RandomAccessFile("src/info/" + fileName + "info.json", "rw");
 		} catch (FileNotFoundException e) {
@@ -302,7 +304,8 @@ public class Storage {
 		}
 		try {
 			infoRaf.seek(0);
-			key = Long.parseLong(infoRaf.readLine());
+			String temp = infoRaf.readLine();
+			key = Long.parseLong(Encryption.Decrypt(mapper.readValue(temp.substring(0,temp.length()-1), String.class), hash.hashCode()));
 			infoRaf.close();
 		} catch (IOException e) {
 			key = 0;
@@ -362,5 +365,29 @@ public class Storage {
 			System.err.println(e.getMessage());
 			System.exit(-1);
 		}
+	}
+	public static void main(String[] args) throws IOException {
+		Storage s1 = new Storage("s1");
+		long x = 123L;
+		long y = 789L;
+		User u1 = new User("a", "12",Role.MODERATOR);
+		User u2 = new User("b", "13",Role.MODERATOR);
+		User u3 = new User("c", "14",Role.MODERATOR);
+		s1.initializeStorageFile(234L,x,y);
+		s1.updateUsers(new User[]{u1,u2,u3});
+		LocalDateTime l = LocalDateTime.of(2022,10,20,20,12,23);
+
+		s1.addMessages(new ChatMessage[]{new ChatMessage(l.minusDays(1),u1,"ilk"),new ChatMessage(l.plusHours(10),u2,"iki"),new ChatMessage(l.plusDays(3),u3,"üç")});
+
+		System.out.println(s1.getSharedKey(x,y));
+		System.out.println(Arrays.toString(s1.getMessages(5)));
+		System.out.println(Arrays.toString(s1.getMessages(l)));
+		System.out.println(s1.getUsers());
+		System.out.println(s1.getChatNames(x,y));
+		//mapper = new ObjectMapper();
+		//mapper.registerModule(new JavaTimeModule());
+		//System.out.println(mapper.writeValueAsString(new ChatMessage(LocalDateTime.parse("2000-01-01T01:01:01"), new User("SportsRoom", "0",Role.MODERATOR), Encryption.Encrypt("This is the start of your conversation",5L))));
+
+		//System.out.println(Encryption.Encrypt("This is the start of your conversation", 5));
 	}
 }
