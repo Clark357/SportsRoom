8a1bdffc0f3b5bd722242257309d1bca8b089106
Mehmet Can Bıyık <can.biyik@ug.bilkent.edu.tr>
2022-12-24 19:40:57 +0300

    Clear up the main method comments

--- src/main/java/org/SportsRoom/Main.java
+++ src/main/java/org/SportsRoom/Main.java
@@ -9,12 +9,8 @@ public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         try{
-            //WARNING: This is not the end code it is for testing only
             UIManager.setLookAndFeel("com.formdev.flatlaf.FlatLightLaf");
             MainWindow n = new MainWindow();
-            while(MetaSuperGroup.metaSuperGroup == null) TimeUnit.MILLISECONDS.sleep(100);
-//            if(MetaSuperGroup.metaSuperGroup.getView().size() != 1)
-//                MetaSuperGroup.initPeerToPeer("Hey Hey", new User[] { new User("mikiyas", input.next(), Role.MODERATOR), new User(MetaSuperGroup.username, MetaSuperGroup.metaSuperGroup.getAddressAsUUID(), Role.WRITER)}, new User[] { new User("mikiyas", input.next(), Role.MODERATOR), new User(MetaSuperGroup.username, MetaSuperGroup.metaSuperGroup.getAddressAsUUID(), Role.WRITER)}, n);
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
