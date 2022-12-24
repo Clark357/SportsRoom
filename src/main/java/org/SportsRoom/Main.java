2d66d4d408477c85c1c854fca23f8bbdb72eb15a
Mehmet Can Bıyık <can.biyik@ug.bilkent.edu.tr>
2022-12-24 19:52:26 +0300

    Little bugfixes to fix the merging bugs

--- src/main/java/org/SportsRoom/Main.java
+++ src/main/java/org/SportsRoom/Main.java
@@ -2,11 +2,14 @@ package org.SportsRoom;
 
 import javax.swing.*;
 
+import java.io.File;
 import java.util.Scanner;
 import java.util.concurrent.TimeUnit;
 
 public class Main {
     public static void main(String[] args) {
+        new File("src/data").mkdirs();
+        new File("src/info").mkdirs();
         Scanner input = new Scanner(System.in);
         try{
             UIManager.setLookAndFeel("com.formdev.flatlaf.FlatLightLaf");
